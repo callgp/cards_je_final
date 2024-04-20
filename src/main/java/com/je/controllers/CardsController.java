93c7e9e644e3d3cc062fd659f95e4c8ffd48e25e
callgp@gmail.com <callgp@gmail.com>
2024-04-20 19:18:04 -0400

    04192020 initial add

--- src/main/java/com/je/controllers/CardsController.java
+++ src/main/java/com/je/controllers/CardsController.java
@@ -1,5 +1,7 @@
 package com.je.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,35 +12,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.je.entities.Cards;
+import com.je.repository.CardsRepository;
+//import com.je.service.CardsService;
 
-import com.je.service.CardsService;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/v1/cards")
 public class CardsController {
 
-	@Autowired
-	private CardsService cardsService;
+	//@Autowired
+	private CardsRepository cardsRepository;
 	
-	@PostMapping
-	public Cards CreateCards(@RequestBody Cards product) {
-		return cardsService.createCards(product);
-	}
+	/*
+	 * @PostMapping public Cards CreateCards(@RequestBody Cards product) { return
+	 * cardsService.createCards(product); }
+	 * 
+	 * @GetMapping("/fetch/{id}") public Cards fetchCards(@PathVariable Integer id)
+	 * { return cardsService.fetchCards(id); }
+	 * 
+	 * @PutMapping("/update/{id}") public Cards updateCards(@PathVariable Integer
+	 * id,@RequestBody Cards product) { return cardsService.updateCards(id,product);
+	 * }
+	 * 
+	 * @DeleteMapping("/delete/{id}") public void
+	 * deleteProduct(@PathVariable(value="id") Integer productId) {
+	 * cardsService.deleteCards(productId); }
+	 */
 	
-	@GetMapping("/fetch/{id}")
-	public Cards fetchCards(@PathVariable Integer id) {
-		return cardsService.fetchCards(id);
-	}
-	
-	@PutMapping("/update/{id}")
-	public Cards updateCards(@PathVariable Integer id,@RequestBody Cards product) {
-		return cardsService.updateCards(id,product);
-	}
-	
-	@DeleteMapping("/delete/{id}")
-	public void deleteProduct(@PathVariable(value="id") Integer productId) {
-		cardsService.deleteCards(productId);
-	}
+	 @GetMapping("/fetch/{customerId}")
+	 public List<Cards> fetchCardDetails(@PathVariable("customerId") int customerId) { 
+		 log.info("acct controler ->fetchCardDetails{}",customerId);
+		 List<Cards> cards= cardsRepository.findByCustomerId(customerId);
+		return cards;
+		 }
 }
--- src/main/java/com/je/repository/CardsRepository.java
+++ src/main/java/com/je/repository/CardsRepository.java
@@ -1,5 +1,7 @@
 package com.je.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.je.entities.Cards;
 
 @Repository //optional
 public interface CardsRepository extends JpaRepository<Cards, Integer>{
+
+	List<Cards> findByCustomerId(int customerId);
+	 
 
 }
